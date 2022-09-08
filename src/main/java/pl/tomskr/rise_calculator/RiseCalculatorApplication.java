04d97e9021ed09b03fb84a6f2b646765e271d04e
TomaszSkrzypek <tomskr3.ts@gmail.com>
2022-09-08 09:14:21 +0200

    Initial commit

---
+++ src/main/java/pl/tomskr/rise_calculator/RiseCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pl.tomskr.rise_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiseCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiseCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/tomskr/rise_calculator/RiseCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomskr.rise_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiseCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
