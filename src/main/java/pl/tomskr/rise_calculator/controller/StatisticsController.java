d7f066047e1dad68c4a60dc53e73d531cc7382b2
TomaszSkrzypek <tomskr3.ts@gmail.com>
2022-11-17 10:02:22 +0100

    Add localhost:3000 acess

--- src/main/java/pl/tomskr/rise_calculator/controller/StatisticsController.java
+++ src/main/java/pl/tomskr/rise_calculator/controller/StatisticsController.java
@@ -2,6 +2,7 @@ package pl.tomskr.rise_calculator.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import pl.tomskr.rise_calculator.service.IRiseService;
 
 import java.util.List;
 
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 @RestController
 public class StatisticsController {
 
